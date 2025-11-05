e414b09950c7ba8d51ffe1fd7c06451f0522108f
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2025-11-06 00:17:35 +0530

    Time: 0 ms (100%), Space: 44.1 MB (5.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,7 +9,27 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+       /* if(head1==null || head2==null){
+            return head1== null?head2:head1;
+        }
+        if(head1.val<=head2.val){
+            head1.next= mergeTwoLists(head1.next,head2);
+            return head1;
+        }
+         if(head2.val<=head1.val){
+            head2.next= mergeTwoLists(head1,head2.next);
+            return head2;*/
+             // base cases
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+
+        if (head1.val <= head2.val) {
+            head1.next = mergeTwoLists(head1.next, head2);
+            return head1;
+        } else {
+            head2.next = mergeTwoLists(head1, head2.next);
+            return head2;
+        }
     }
 }
