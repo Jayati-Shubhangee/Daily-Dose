da3948e7e9cff2497ed954b2728b7f4f3326e7eb
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-01-10 15:45:43 +0530

    Time: 18 ms (80.44%), Space: 98.3 MB (9.27%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,27 @@
+class Solution {
+    static void solve(int start, int idx,int n , int k, List<Integer> combo, List<List<Integer>> ans){
+        //base case
+        if(idx==k){
+            ans.add(new ArrayList<>(combo));
+            return;
+        }
+        if(start > n) return ;
+        combo.add(start);
+        //recursive calll
+        solve(start+1, idx+1,n,k,combo,ans);
+        //backtrack removal
+        combo.remove(combo.size()-1);
+        //removal 
+        solve(start+1,idx,n,k,combo,ans);
+
+    }
+    public List<List<Integer>> combine(int n, int k) {
+       List<List<Integer>> ans= new ArrayList<>() ;
+       List<Integer> combo= new ArrayList<>();
+       int start=1;
+       int idx=0;
+       solve(start,idx,n,k,combo,ans);
+       return ans;
+
+    }
+}
