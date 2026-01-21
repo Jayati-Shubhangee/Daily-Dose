be75fe035e7cf7acea83ffd8b335661b6892fd39
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-01-21 13:02:31 +0530

    Time: 277 ms (31.62%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT MAX(salary) AS SecondHighestSalary
+FROM (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS temp;
+
