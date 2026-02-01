cf160ba60a8bcc4b38b30616fbf025714a3c0453
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-01 22:48:01 +0530

    Time: 11 ms (9.73%), Space: 46.8 MB (11.87%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+         List<TreeNode> pathP = new ArrayList<>();
+        List<TreeNode> pathQ = new ArrayList<>();
+
+        // find paths
+        findPath(root, p, pathP);
+        findPath(root, q, pathQ);
+
+        TreeNode lca = null;
+
+        // compare paths
+        int i = 0;
+        while (i < pathP.size() && i < pathQ.size()) {
+            if (pathP.get(i) == pathQ.get(i)) {
+                lca = pathP.get(i);
+            } else {
+                break;
+            }
+            i++;
+        }
+
+        return lca;
+    }
+
+    // DFS to find path from root to target
+    private boolean findPath(TreeNode root, TreeNode target, List<TreeNode> path) {
+        if (root == null) return false;
+
+        path.add(root);
+
+        if (root == target) return true;
+
+        if (findPath(root.left, target, path) ||
+            findPath(root.right, target, path)) {
+            return true;
+        }
+
+        // backtrack
+        path.remove(path.size() - 1);
+        return false;
+    }
+}
