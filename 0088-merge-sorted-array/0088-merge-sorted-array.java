e50d5b4b4e9e19652c35e1a1d25601743706d03f
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-02 15:09:41 +0530

    Time: 1 ms (24.38%), Space: 43.8 MB (55.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
         nums1=nums2;
             System.out.println(nums1);
         }*/
-            int i =m-1;
+           /* int i =m-1;
             int j =n-1;
             int k = m+n-1;
             if(m==0 && n!=0){
@@ -35,6 +35,36 @@ class Solution {
              //Arrays.sort(nums1);
        // System.out.println(nums1);
         
-       
+       */
+       int i = 0; // pointer for nums1
+        int j = 0; // pointer for nums2
+
+        while (i < m && j < n) {
+
+            // if element in nums2 should come before nums1[i]
+            if (nums2[j] < nums1[i]) {
+
+                // shift nums1 elements one step right
+                for (int k = m; k > i; k--) {
+                    nums1[k] = nums1[k - 1];
+                }
+
+                // insert nums2[j] at position i
+                nums1[i] = nums2[j];
+
+                j++;   // move nums2 pointer
+                m++;   // nums1 size increases
+                i++;   // move nums1 pointer
+            } else {
+                i++;   // move nums1 pointer only
+            }
+        }
+
+        // if nums2 still has elements left, append them
+        while (j < n) {
+            nums1[m] = nums2[j];
+            m++;
+            j++;
     }
+}
 }
