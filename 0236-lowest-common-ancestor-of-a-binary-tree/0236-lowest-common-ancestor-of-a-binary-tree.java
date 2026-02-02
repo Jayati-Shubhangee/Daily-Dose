6c96713430c71b6429d436ebac691b9bb38762b8
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-02 17:15:03 +0530

    Time: 7 ms (98.78%), Space: 46.6 MB (37.48%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,7 +9,7 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-         List<TreeNode> pathP = new ArrayList<>();
+        /* List<TreeNode> pathP = new ArrayList<>();
         List<TreeNode> pathQ = new ArrayList<>();
 
         // find paths
@@ -47,6 +47,22 @@ class Solution {
 
         // backtrack
         path.remove(path.size() - 1);
-        return false;
+        return false;*/
+        if(root==null){
+            return null;
+        }
+        if(root==p || root==q){
+            return root;
+        }
+        TreeNode leftLCA=lowestCommonAncestor(root.left,p,q);
+        TreeNode rightLCA=lowestCommonAncestor(root.right,p,q);
+        if(leftLCA!=null && rightLCA!=null){
+            return root;
+        }
+        else if(leftLCA !=null){
+            return leftLCA;
+        }
+        else
+        return rightLCA;
     }
 }
