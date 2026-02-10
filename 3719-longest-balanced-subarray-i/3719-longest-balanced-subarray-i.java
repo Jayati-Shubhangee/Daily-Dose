2fa1e1398f6fe309635734b8925de1b1fa000138
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-10 21:30:46 +0530

    Time: 299 ms (77.55%), Space: 47.1 MB (45.62%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestBalanced(int[] nums) {
+        //BRUTE FORCE APPROACH 
+        int maxlen = 0;
+        for(int i=0;i<nums.length;i++){
+            HashSet<Integer> odd= new HashSet<>();
+             HashSet<Integer> even= new HashSet<>();
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]%2==0){
+                    even.add(nums[j]);
+                }
+                else{
+                    odd.add(nums[j]);
+                }
+                if(odd.size()==even.size()){
+                   maxlen= Math.max(maxlen,j-i+1);
+                }
+            }
+        }
+        return maxlen;
+    }
+}
