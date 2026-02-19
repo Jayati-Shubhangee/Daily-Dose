03721900134b319054e2fe2cb9b5be64d84a2243
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-19 20:34:51 +0530

    Time: 3 ms (26.77%), Space: 72.2 MB (42.05%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,50 +1,84 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       int product=1;
-        for(int i=0;i<nums.length;i++){
-            product*=nums[i];
-
+       int[] ans = new int[nums.length];
+       int prod=1;
+       int n = nums.length;
+      /* for(int i=0;i<n;i++){
+        prod=1;
+        if(i!= 0 && i!=n-1 ){
+            for(int j =0 ; j<i;j++){//forward
+                prod*=nums[j];
+            }
+             for(int j =n-1 ; j>i;j--){//backward
+                prod*=nums[j];
+            }
+            ans[i]=prod;
         }
-
-        int[] result= new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-
-            if(nums[i]==0 && i==nums.length-1){
-                int prod=1;
-                for(int j=0;j<nums.length-1;j++){
-                    prod*=nums[j];
-                }
-                result[i]=prod;
-            
+        else if(i==0){
+            //only forward
+            for(int j =1 ; j<n;j++){//forward
+                prod*=nums[j];
             }
-            else if(nums[i]==0 && i==0){
-               int prod=1;
-                for(int j=1;j<nums.length;j++){
-                    prod*=nums[j];
-                }
-                result[i]=prod;
-
-            }
-            else if(nums[i]==0 && i!=0 && i!=nums.length-1){
-                int left=1;
-                int right=1;
-                 int prod=1;
-              
-                for(int j=i-1;j>=0;j--){
-                    left*=nums[j];
-                }
-                for(int j=i+1;j<nums.length;j++){
-                    right*=nums[j];
-                }
-                prod=left*right;
-                result[i]=prod;
-
-
-            }
-            else{
-            result[i]=product/nums[i];
-            }
+            ans[i]=prod;
         }
-        return result;  
+        else if(i==n-1){
+            //only backward
+            for(int j =n-2 ; j>=0;j--){
+                prod*=nums[j];
+            }
+            ans[i]=prod;
+        }
+        }
+        return ans;*/
+// for edge case zero handlig 
+//search if any zero exist
+//int found=0;
+int count=0;
+int pos=0;
+for(int i=0;i< n ;i++){
+    if(nums[i]==0){
+        pos=i;
+       // found=1;
+        count++;
+    }
+
+}
+
+if(count>1){
+    for(int i=0;i<n;i++){
+        ans[i]=0;
     }
 }
+else if(count==1){
+    for(int i=0;i<pos;i++){
+        prod*=nums[i];
+    }
+    for(int j=n-1;j>pos;j--){
+        prod*=nums[j];
+    }
+
+
+
+for(int i=0;i<n;i++){
+    if(i!=pos)
+    ans[i]=0;
+    else{
+        ans[i]=prod;
+    }
+}
+}
+
+ 
+ else{
+        for(int i =0;i< n ;i++){
+            prod*=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+            ans[i]=prod/nums[i];
+        }
+       
+       }
+        return ans;
+    }
+    }
