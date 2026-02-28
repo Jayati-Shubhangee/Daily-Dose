158470ec00db02c42c564230925b76022508ea37
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-28 12:43:42 +0530

    Time: 34 ms (16.99%), Space: 47.2 MB (38.78%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int firstUniqChar(String s) {
+      //brute force apporach 
+    /*  int ans=0;
+      int i=0;
+      int found=0;
+      for( i=0;i<s.length()-1;i++){
+            found=0;
+        for(int j=0;j<i;j++){
+            if(s.charAt(i)==s.charAt(j)){
+                found=1;
+            }
+        }
+        for(int j=i+1;j<s.length();j++){
+             if(s.charAt(i)==s.charAt(j)){
+                found=1;
+            }
+        }
+        
+      }
+      if(i==s.length()-1 && found==-1){
+            for(int j=0;j<s.length()-1;j++){
+                if(s.charAt(j)==s.charAt(i)){
+                found=1;
+                }
+            }
+      }
+        if(found==0){
+            ans=i;
+            break;
+        }
+        else if(found==1){
+            ans=-1;
+        }
+         return ans;*/
+          for(int i = 0; i < s.length(); i++) {
+
+            int found = 0;
+
+            // check left side
+            for(int j = 0; j < i; j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            // check right side only if not found yet
+            if(found == 0) {
+                for(int j = i + 1; j < s.length(); j++) {
+                    if(s.charAt(i) == s.charAt(j)) {
+                        found = 1;
+                        break;
+                    }
+                }
+            }
+
+            if(found == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+      }
+     
+        }
+
+      
+    
