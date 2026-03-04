7fb6305c84fc30fa22ef4b5496ff9a38af72c15d
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-03-04 18:29:04 +0530

    Time: 1 ms (87.93%), Space: 44.4 MB (37.72%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-     List<List<Integer>> ans= new ArrayList<>();
-     List<Integer>input= new ArrayList<>();
-     for(int i=0;i<nums.length;i++){
-        input.add(nums[i]);
-     }
-     solve(input,new ArrayList<>(),ans);
-     return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        int idx=0;
+        solve(nums,idx,new ArrayList<>(),ans);
+        return ans;
     }
-    static void solve(List<Integer> input, List<Integer> output,List<List<Integer>> ans){
-        if(input.size()==0){
-            ans.add(new ArrayList<>(output));
-            return;
+    public void solve(int[] nums, int idx,List<Integer> curr,List<List<Integer>>ans){
+        ///base case 
+        if(idx==nums.length){
+            //adding answer or separte arraylist created in the final array list ans
+ans.add(new ArrayList<>(curr));
+return;
         }
-        int ele= input.get(0);
-        input.remove(0);
-        solve(input,output,ans);
-        input.add(0,ele);
-        output.add(ele);
-        input.remove(0);
-        solve(input,output,ans);
-        input.add(0,ele);
-        output.remove(output.size()-1);
+        //pick part 
+        curr.add(nums[idx]);
+        solve(nums,idx+1,curr,ans );
+        //backtrack
+        curr.remove(curr.size()-1);
+        // not pick part 
+        solve(nums,idx+1,curr,ans);
     }
 }
