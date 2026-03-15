0bd22cec2351e47eef5aaec79f445dc5cb724eeb
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-03-15 11:37:44 +0530

    Time: 34 ms (8.26%), Space: 44.8 MB (48.8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //edge case 
+       /* //edge case 
        
         StringBuilder sb = new StringBuilder(s);
         /* if(sb.length()==1 &&  Character.isLetterOrDigit(sb.charAt(0))==false){
@@ -10,7 +10,7 @@ class Solution {
             return true;
         }*/
 
-        for(int i=0;i<sb.length();i++){
+        /*for(int i=0;i<sb.length();i++){
             char ch = sb.charAt(i);
             if(Character.isUpperCase(ch)){
               sb.setCharAt(i,Character.toLowerCase(ch));
@@ -24,6 +24,19 @@ class Solution {
         r.reverse();
         String original=sb.toString();
         String rev=r.toString();
-        return(original.equals(rev));
+        return(original.equals(rev));*/
+
+
+        String str=s.toLowerCase();
+        StringBuilder sb = new StringBuilder(str);
+        for(int i=0;i<sb.length();i++){
+            if(!(Character.isDigit(sb.charAt(i)) || Character.isLetter(sb.charAt(i)))){
+                sb.deleteCharAt(i);
+                i--;
+            }
+        }
+        StringBuilder rev=new StringBuilder(sb);
+        rev.reverse();
+        return(rev.toString().equals(sb.toString()));
     }
 }
